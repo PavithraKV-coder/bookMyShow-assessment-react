99e1104e8878124600e945bb1b66aa428b3cb3bd
PavithraKV-coder <61187544+PavithraKV-coder@users.noreply.github.com>
2020-03-10 15:02:41 +0530

    Create SearchForm.js

---
+++ src/components/home/SearchForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import {
+  searchMovie,
+  fetchMovies,
+  setLoading
+} from '../../actions/fetchActions';
+
+export class SearchForm extends Component {
+  onChange = e => {
+      console.log(e.target.value)
+    this.props.searchMovie(e.target.value);
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.props.text)
+    this.props.fetchMovies(this.props.text);
+    this.props.setLoading();
+  };
+
+  render() {
+    return (
+      <div className="mb-4">
+        <form id="searchForm" className="row" onSubmit={this.onSubmit}>
+            <div className="col-md-4">
+                <input
+                    type="text"
+                    className="form-control"
+                    name="searchText"
+                    placeholder="Search for Movies ..."
+                    onChange={this.onChange}
+                />
+            </div>
+            <div className="col-md-4">
+                <button type="submit" className="btn btn-primary btn-bg">
+                    Search
+                </button>
+            </div>
+            
+        </form>
+    </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  text: state.movies.text
+});
+
+export default connect(
+  mapStateToProps,
+  { searchMovie, fetchMovies, setLoading }
+)(SearchForm);
